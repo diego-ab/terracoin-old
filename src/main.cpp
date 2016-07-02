a21a21a370eafe07adda88b167455c90ccdb62ee
Ahmed Bodiwala <ahmed_bodi@msn.com>
2016-07-02 02:42:06 +0100

    Set Block Subsidies for TRC

--- src/main.cpp
+++ src/main.cpp
@@ -1691,11 +1691,7 @@ bool ReadBlockFromDisk(CBlock& block, const CBlockIndex* pindex, const Consensus
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
     int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
-    // Force block reward to zero when right shift is undefined.
-    if (halvings >= 64)
-        return 0;
-
-    CAmount nSubsidy = 50 * COIN;
+    CAmount nSubsidy = 20 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
