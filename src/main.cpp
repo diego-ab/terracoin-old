52329975ca75f3e95353800960d4e018304dcb17
Ahmed Bodiwala <ahmed_bodi@msn.com>
2016-07-02 16:10:20 +0100

    Set Terraoin Message signing name

--- src/main.cpp
+++ src/main.cpp
@@ -118,7 +118,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "Bitcoin Signed Message:\n";
+const string strMessageMagic = "Terracoin Signed Message:\n";
 
 // Internal stuff
 namespace {
