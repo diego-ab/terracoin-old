065a9b0b09d82528873d1f9fc67f4124601b8675
Ahmed Bodiwala <ahmed_bodi@msn.com>
2016-07-09 19:56:22 +0100

    Changed Version String. SatoshiClone -> Terracoin

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both bitcoind and bitcoin-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("SatoshiClone");
+const std::string CLIENT_NAME("Terracoin");
 
 /**
  * Client version number
